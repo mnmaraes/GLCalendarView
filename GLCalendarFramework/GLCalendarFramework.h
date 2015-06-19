8a5dd073b78c98c7e6d0ef0adb292887bf2fb204
Murillo Nicacio de Maraes <mnmaraes@gmail.com>
2015-06-19 02:26:23 -0700

    Copy From The Original Repo + A Few Small Changes:
    1. The outstanding pull request for internationalization has been added.
    2. GLDateUtils is now able to calculate the last day of a given month.
    3. Adds the ability to annotate date cells by adding a DataSource
    Protocol.

---
+++ GLCalendarFramework/GLCalendarFramework.h
@@ -0,0 +1,24 @@
+//
+//  GLCalendarFramework.h
+//  GLCalendarFramework
+//
+//  Created by Murillo Nicacio de Maraes on 6/19/15.
+//  Copyright (c) 2015 Unreasonable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GLCalendarFramework.
+FOUNDATION_EXPORT double GLCalendarFrameworkVersionNumber;
+
+//! Project version string for GLCalendarFramework.
+FOUNDATION_EXPORT const unsigned char GLCalendarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLCalendarFramework/PublicHeader.h>
+
+#import <GLCalendarFramework/GLCalendarDateRange.h>
+#import <GLCalendarFramework/GLCalendarDayCell.h>
+#import <GLCalendarFramework/GLCalendarDayCellBackgroundCover.h>
+#import <GLCalendarFramework/GLCalendarMonthCoverView.h>
+#import <GLCalendarFramework/GLDateUtils.h>
+#import <GLCalendarFramework/GLCalendarView.h>
